471da26557d680cef344d85428e5cce9633e937a
Flavio Di Natale <dinataleflavio@gmail.com>
2025-03-26 20:54:26 +0100

    Move handler

---
+++ helipad/handlers.py
@@ -0,0 +1,47 @@
+import logging
+import datetime
+
+logger = logging.getLogger(__name__)
+
+
+class MessageHandler:
+    def __init__(self):
+        self.aircrafts = dict()
+
+    def handle_message(self, message):
+        segments = message.split(",")
+
+        # filter out unexpected messagges
+        if segments[0] != "MSG":
+            logger.warning(f"Got an unrecognized message: {message}")
+            return
+        if segments[1] == "7":
+            logger.debug("Ignoring message of type 7")
+            return
+        if segments[2] != "111" or segments[3] != "11111" or segments[5] != "111111":
+            logger.warning(
+                f"Got a message with unexpected values in fields 3,4 or 6: {message}"
+            )
+            return
+
+        timestamp_format = "%Y/%m/%d%H:%M:%S.%f"
+        timestamp = datetime.datetime.strptime(
+            f"{segments[8]}{segments[9]}", timestamp_format
+        )
+        message_type = segments[1]
+        aircraft_id = segments[4]
+        relevant_segments = []
+        for field_index in range(10, 22):
+            if segments[field_index] != "":
+                relevant_segments.append((field_index, segments[field_index]))
+        print(f"MSG {timestamp} {message_type} {aircraft_id} {relevant_segments}")
+
+
+# class DumpHandler:
+#     def __init__(self, filename=None):
+#         self.dump_file = open(filename or self.__class__.default_filename(), 'w'
+
+
+#     def default_filename():
+#         now = datetime.datetime.now(datetime.UTC).strftime("%Y%m%d_%H%M%S")
+#         return f"{now}_dump.txt"
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import datetime
 
 import helipad.config.logging
 from helipad.streams import StreamReader
-
+from helipad.handlers import MessageHandler
 
 helipad.config.logging.init_logging()
 logger = logging.getLogger(__name__)
@@ -30,39 +30,6 @@ def main():
             dump.write(f"{message}\n")
             dump.flush()
             handler.handle_message(message)
-
-
-class MessageHandler:
-    def __init__(self):
-        self.aircrafts = dict()
-
-    def handle_message(self, message):
-        segments = message.split(",")
-
-        # filter out unexpected messagges
-        if segments[0] != "MSG":
-            logger.warning(f"Got an unrecognized message: {message}")
-            return
-        if segments[1] == "7":
-            logger.debug("Ignoring message of type 7")
-            return
-        if segments[2] != "111" or segments[3] != "11111" or segments[5] != "111111":
-            logger.warning(
-                f"Got a message with unexpected values in fields 3,4 or 6: {message}"
-            )
-            return
-
-        timestamp_format = "%Y/%m/%d%H:%M:%S.%f"
-        timestamp = datetime.datetime.strptime(
-            f"{segments[8]}{segments[9]}", timestamp_format
-        )
-        message_type = segments[1]
-        aircraft_id = segments[4]
-        relevant_segments = []
-        for field_index in range(10, 22):
-            if segments[field_index] != "":
-                relevant_segments.append((field_index, segments[field_index]))
-        print(f"MSG {timestamp} {message_type} {aircraft_id} {relevant_segments}")
 
 
 if __name__ == "__main__":
