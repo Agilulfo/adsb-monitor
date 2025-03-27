9b64864d66c748fd8ac0d37c711da173d33a744a
Flavio Di Natale <dinataleflavio@gmail.com>
2025-03-27 12:26:23 +0100

    Add argument parsing

--- run.py
+++ run.py
@@ -1,17 +1,54 @@
-import helipad.config.logging
-from helipad.streams import StreamReader
-# from helipad.streams import FileReader
+import argparse
+import helipad.config.logging as app_logging
+
+from helipad.streams import StreamReader, FileReader
 from helipad.handlers import MessageHandler, DumpHandler
 
-helipad.config.logging.init_logging()
+app_logging.init_logging()
+
+
+def init_parser():
+    parser = argparse.ArgumentParser()
+
+    # source options
+    parser.add_argument(
+        "-p", "--port", type=int, help="port for the socket", default=30003
+    )
+    parser.add_argument(
+        "-H", "--host", help="address for the socket", default="localhost"
+    )
+    parser.add_argument("-f", "--fromfile", help="load data from dumpfile")
+
+    # dump creation options
+    parser.add_argument("-d", "--dump", help="produce a dump file", action="store_true")
+    parser.add_argument("-D", "--dumpto", help="produce a dump to a specific file")
+
+    return parser
 
 
 def main():
-    stream = StreamReader("localhost", 30003)
-    # stream = FileReader("./relevant_dumps/20250326_124923_dump.txt")
+    # parse CLI arguments
+    parser = init_parser()
+    args = parser.parse_args()
 
-    handlers = [DumpHandler(), MessageHandler()]
+    # Pick stream
+    stream = None
+    if args.fromfile:
+        stream = FileReader(args.fromfile)
+    else:
+        stream = StreamReader(args.host, args.port)
 
+    # build handlers
+    handlers = []
+
+    # optional handlers
+    if args.dump or args.dumpto:
+        handlers.append(DumpHandler(args.dumpto))
+
+    # mandatory handlers
+    handlers.append(MessageHandler())
+
+    # main loop
     while True:
         message = stream.read_line()
         for handler in handlers:
