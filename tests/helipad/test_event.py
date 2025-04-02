0eb3ee66647359575efeea0e165d0cd00f56c840
Flavio Di Natale <dinataleflavio@gmail.com>
2025-04-02 10:27:47 +0200

    fix test

--- tests/helipad/test_event.py
+++ tests/helipad/test_event.py
@@ -2,5 +2,5 @@ from helipad.events import Event
 
 
 def test_event_creation(load_pois):
-    Event({"type": "landing", "poi": "Warsaw Chopin"})
+    event = Event({"type": "landing", "poi": "Warsaw Chopin"})
     assert repr(event) == "EVENT: landing - Warsaw Chopin"
